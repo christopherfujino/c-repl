4f0a4af2e51b344ea512f080e97d5690ab5a15e1
Christopher Fujino <christopherfujino@gmail.com>
2026-02-13 23:03:16 -0800

    clean

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 #include "ffi.h"
 
 void dynamicCall(void (*fn)(void), int argc, void **argv, ffi_type **argTypes,
-                 void *retValue) {
+                 void *retValue, ffi_type *retType) {
   ffi_cif cif;
   ffi_status status =
-      ffi_prep_cif(&cif, FFI_DEFAULT_ABI, argc, &ffi_type_sint /* TODO */, argTypes);
+      ffi_prep_cif(&cif, FFI_DEFAULT_ABI, argc, retType, argTypes);
   if (status != FFI_OK) {
     fprintf(stderr, "Failed to call ffi_prep_cif\n");
     exit(1);
@@ -24,29 +24,23 @@ int main(void) {
     return 1;
   }
 
-  int (*_puts)(const char *);
-  void *rawPuts = dlsym(self, "puts");
+  int (*_puts)(const char *) = NULL;
   // Hack to satisfy -Wpedantic:
   // https://pubs.opengroup.org/onlinepubs/009695399/functions/dlsym.html
-  *(void **)(&_puts) = rawPuts;
-  _puts("Hello, world!");
-
-  // void ffi_call (ffi_cif *cif, void *fn, void *rvalue, void **avalues)
-  //
-  // rvalue is a pointer to a chunk of memory that will hold the result of the
-  // function call. This must be large enough to hold the result, no smaller
-  // than the system register size (generally 32 or 64 bits), and must be
-  // suitably aligned; it is the caller’s responsibility to ensure this. If
-  // cif declares that the function returns void (using ffi_type_void), then
-  // rvalue is ignored.
+  *(void **)(&_puts) = dlsym(self, "puts");
+  if (_puts == NULL) {
+    fprintf(stderr, "Failed to dlsym()\n");
+    return 1;
+  }
 
   const char *arg = "Hello from libffi!";
-  void* args[1] = {(void *)&arg};
-  //void *args[1];
-  //args[0] = &arg;
+  void *args[1] = {(void *)&arg};
   ffi_type *argTypes[1] = {&ffi_type_pointer};
+
+  // Caller must ensure this is big enough to hold the return value
   ffi_arg retValue;
-  dynamicCall((void (*)(void))_puts, 1, args, argTypes, &retValue);
+  dynamicCall((void (*)(void))_puts, 1, args, argTypes, &retValue,
+              &ffi_type_sint);
 
   // TODO check retValue
 }
